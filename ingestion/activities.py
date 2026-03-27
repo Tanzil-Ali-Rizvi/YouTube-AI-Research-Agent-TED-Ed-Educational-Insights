b8a7b3f156310c9f789d28c3d9174a0706ab569d
Tanzil-Ali-Rizvi <tanzilali.rizvi@gmail.com>
2026-03-28 00:52:24 +0530

    Update activities.py

--- ingestion/activities.py
+++ ingestion/activities.py
@@ -1,22 +1,17 @@
-import os
-from temporalio import activity
-from youtube_transcript_api import YouTubeTranscriptApi
-from elasticsearch import Elasticsearch
-
-class YoutubeActivities:
-    def __init__(self, es_client: Elasticsearch):
-        self.es = es_client
-
-    @activity.def
-    async def fetch_transcript(self, video_id: str) -> str:
-        # Configurating for Proxy (essential for TED-Ed bulk scraping)
-        proxy = {
-            "http": f"http://{os.getenv('PROXY_USER')}:{os.getenv('PROXY_PASSWORD')}@{os.getenv('PROXY_BASE_URL')}",
-            "https": f"http://{os.getenv('PROXY_USER')}:{os.getenv('PROXY_PASSWORD')}@{os.getenv('PROXY_BASE_URL')}"
-        }
-        transcript_list = YouTubeTranscriptApi.get_transcript(video_id, proxies=proxy)
-        return " ".join([t['text'] for t in transcript_list])
-
-    @activity.def
-    async def index_document(self, data: dict):
-        self.es.index(index="ted_ed_content", id=data['video_id'], document=data)
+@activity.def
+async def index_semantic_document(self, data: dict):
+    # Initializing the tools class to use the embedding logic
+    tools = TedEdSemanticTools() 
+    
+    # Generating the vector for the transcript
+    vector = tools.get_embedding(data['text'])
+    
+    # Indexing both the text and the vector
+    document = {
+        "video_id": data['video_id'],
+        "title": data['title'],
+        "text": data['text'],
+        "text_vector": vector  # This is the 'semantic' heart of the project
+    }
+    
+    self.es.index(index="ted_ed_semantic", id=data['video_id'], document=document)
